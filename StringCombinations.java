d60783ba87a643dfa38f33d3b8fe4dbc3197a6e2
Jay Sohani <127607690+thejzany@users.noreply.github.com>
2023-08-30 23:56:12 +0530

    String Based Logical Programs

--- StringCombinations.java
+++ StringCombinations.java
@@ -5,7 +5,7 @@ public class StringCombinations {
 	public static void main(String[] args) {
 		String s = "jaysohani";
 		
-		for(int i=0; i<=s.length();i++) {
+		for(int i=0; i<s.length();i++) {
 			for(int j=i+1;j<=s.length();j++) {
 				System.out.println(s.substring(i,j));
 			}
